5ea51bf7dd3a1583147818aff9665feb15bf2e46
Kerem_381 <217024091+Kerem3801@users.noreply.github.com>
2026-09-03 11:23:15 +0300

    Remove fastbootd configuration from device.mk
    Removed fastbootd related packages and properties.

--- device.mk
+++ device.mk
@@ -14,13 +14,6 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 # No dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := false
 
-# fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
-PRODUCT_PRODUCT_PROPERTIES += \
-	ro.fastbootd.available=true
 
 # Screen
 TARGET_SCREEN_WIDTH := 1080
